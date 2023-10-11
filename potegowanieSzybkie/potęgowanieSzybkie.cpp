d111735a718d52f9cc2c87f6aa22dae5fa7ee354
Oceanenjoyer <94893774+Oceanenjoyer@users.noreply.github.com>
2023-10-11 10:45:06 +0200

    dodano potegowanieSzybkie

---
+++ potegowanieSzybkie/potęgowanieSzybkie.cpp
@@ -0,0 +1,27 @@
+﻿using namespace std;
+#include <iostream>
+
+int toPower(int a, int n) {
+    int w = 1;
+    while (n > 0)
+    {
+        if (n % 2 == 1) w *= a;
+        a *= a;
+        n /= 2;
+    }
+    return w;
+}
+
+int main()
+{
+    int a;
+    int n;
+    cout << "Podaj podstawe: ";
+    cin >> a;
+
+    cout << "Podaj wykladnik: ";
+    cin >> n;
+
+    int val = toPower(a, n);
+    cout << a << " do potegi " << n << " wynosi: " << val;
+}
