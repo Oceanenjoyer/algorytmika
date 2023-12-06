c6e9a8f438d5a4451cce1b01df0aa49e1b46d3fd
Oceanenjoyer <94893774+Oceanenjoyer@users.noreply.github.com>
2023-12-06 10:57:10 +0100

    dodano n-ty-wyraz-ciagu

---
+++ n-ty-wyraz-ciagu/N-ty_wyraz_ciągu.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+float recursive(int n) {
+	if (n == 1) {
+		return 1;
+	}
+
+	if (n == 2) {
+		return 0.5;
+	}
+
+	return -recursive(n - 1) * recursive(n - 2);
+}
+
+int main() {
+	int n;
+
+	std::cout << "Podaj nr wyrazu ciagu, ktorego wartosc chcesz policzyc: ";
+	std::cin >> n;
+
+	std::cout << n << " wyraz ciagu ma wartosc " << recursive(n);
+}
